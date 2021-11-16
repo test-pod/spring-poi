562b316579bbb0439a2d9b70b7b1d3132bdb0c65
weishang <1012691657@qq.com>
2021-11-16 09:24:24 +0800

    2021-11-16 1. 无需读写分离。

--- src/main/java/com/shang/poi/SpringPoiApplication.java
+++ src/main/java/com/shang/poi/SpringPoiApplication.java
@@ -14,10 +14,7 @@ import org.springframework.context.annotation.Bean;
 
 import javax.annotation.Resource;
 import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -27,8 +24,6 @@ public class SpringPoiApplication {
     private static final String BATCH_NO = "20211112203136794";
 
     private static final Integer PAGE_SIZE = 1000;
-
-    private static final ArrayBlockingQueue<MockResultPlayBack> CACHE = new ArrayBlockingQueue<>(10000);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringPoiApplication.class, args);
@@ -40,46 +35,17 @@ public class SpringPoiApplication {
     @Bean
     public CommandLineRunner commandLineRunner() {
         return args -> {
-            final AtomicBoolean readComplete = new AtomicBoolean(false);
-            final CountDownLatch latch = new CountDownLatch(1);
-            new Thread(() -> {
-                try {
-                    final AtomicInteger pageNum = new AtomicInteger(1);
-                    final AtomicBoolean hasNextPage = new AtomicBoolean(true);
-                    do {
-                        final PageInfo<MockResultPlayBack> pageInfo = mockResultPlayBackService.listByBatchNoAndPage(BATCH_NO, pageNum.getAndIncrement(), PAGE_SIZE);
-                        hasNextPage.set(pageInfo.isHasNextPage());
-                        final List<MockResultPlayBack> mockResultPlayBacks = pageInfo.getList();
-                        for (final MockResultPlayBack mockResultPlayBack : mockResultPlayBacks) {
-                            CACHE.put(mockResultPlayBack);
-                        }
-                    } while (hasNextPage.get());
-                    readComplete.set(true);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }).start();
-            new Thread(() -> {
-                try {
-                    final ExcelWriter writer = EasyExcel.write(Paths.get("Mock.xlsx").toFile(), MockResultPlayBackVo.class).build();
-                    final WriteSheet sheet = EasyExcel.writerSheet("Mock").build();
-                    while (!readComplete.get()) {
-                        final MockResultPlayBack mockResultPlayBack = CACHE.take();
-                        writer.write(Collections.singleton(mockResultPlayBack), sheet);
-                    }
-                    if (CACHE.size() > 0) {
-                        for (MockResultPlayBack mockResultPlayBack : CACHE) {
-                            writer.write(Collections.singleton(mockResultPlayBack), sheet);
-                        }
-                    }
-                    writer.finish();
-                    latch.countDown();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }).start();
-
-            latch.await();
+            final AtomicInteger pageNum = new AtomicInteger(1);
+            final AtomicBoolean hasNextPage = new AtomicBoolean(true);
+            final ExcelWriter writer = EasyExcel.write(Paths.get("Mock.xlsx").toFile(), MockResultPlayBackVo.class).build();
+            final WriteSheet sheet = EasyExcel.writerSheet("Mock").build();
+            do {
+                final PageInfo<MockResultPlayBack> pageInfo = mockResultPlayBackService.listByBatchNoAndPage(BATCH_NO, pageNum.getAndIncrement(), PAGE_SIZE);
+                hasNextPage.set(pageInfo.isHasNextPage());
+                final List<MockResultPlayBack> mockResultPlayBacks = pageInfo.getList();
+                writer.write(mockResultPlayBacks, sheet);
+            } while (hasNextPage.get());
+            writer.finish();
             System.exit(0);
         };
     }
